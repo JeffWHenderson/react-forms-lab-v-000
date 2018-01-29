51613fabcd4395be109f6a0f3df3fa1d6b80f89a
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-29 20:15:57 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,10 +3,15 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor(props) {
     super(props);
+    
     this.handleChange = this.handleChange.bind(this)
     this.state = { value: '' };
   }
 
+  handleChange(event) {
+    this.setState({value: e.target.value})
+  }
+  
   render() {
     return (
       <div>
